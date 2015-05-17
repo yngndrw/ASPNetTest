255e71351c92fc311984bc279eb03e8ee78c9e95
Andrew Young <yngndrw@gmail.com>
2015-05-17 03:30:14 +0100

    Revert "Caching test #2"
    This reverts commit 4b202df6414b633d58e95a2cf79af5dd6a74e1c6.

--- src/ASPNetTest/Startup.cs
+++ src/ASPNetTest/Startup.cs
@@ -8,15 +8,16 @@ using Microsoft.Framework.DependencyInjection;
 
 namespace ASPNetTest
 {
-	public class Startup
-	{
-		public void ConfigureServices(IServiceCollection services)
-		{
-		}
+    public class Startup
+    {
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
 
-		public void Configure(IApplicationBuilder app)
+        public void Configure(IApplicationBuilder app)
 		{
 			app.UseWelcomePage();
-		}
-	}
+        }
+    }
 }
