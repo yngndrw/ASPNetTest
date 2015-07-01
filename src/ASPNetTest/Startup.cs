461804e45f56bbc785ad2ec450b534a575424e0f
Andrew Young <yngndrw@gmail.com>
2015-07-01 22:53:42 +0100

    Upgraded aspnet dependencies to beta5

--- src/ASPNetTest/Startup.cs
+++ src/ASPNetTest/Startup.cs
@@ -1,21 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNet.Builder;
-using Microsoft.AspNet.Http;
-using Microsoft.Framework.DependencyInjection;
+﻿using Microsoft.AspNet.Builder;
+using Microsoft.Framework.Logging;
 
 namespace ASPNetTest
 {
 	public class Startup
 	{
-		public void ConfigureServices(IServiceCollection services)
+		public void Configure(IApplicationBuilder app, ILoggerFactory loggerFactory)
 		{
-		}
-
-		public void Configure(IApplicationBuilder app)
-		{
+			loggerFactory.AddConsole();
+			app.UseStaticFiles();
 			app.UseWelcomePage();
 		}
 	}
